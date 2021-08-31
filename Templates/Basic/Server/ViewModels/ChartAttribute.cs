9b15f69c0c7e283f9ab030b5f4ac8205f6570ae6
tar-dev <73483060+tar-dev@users.noreply.github.com>
2021-08-31 09:30:35 +0700

    upload

---
+++ Templates/Basic/Server/ViewModels/ChartAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace Basic.Server
+{
+    internal class ChartAttribute
+    {
+        public string XAxisLabel { get; set; }
+        public string YAxisLabel { get; set; }
+        public int MaxDataSize { get; set; }
+    }
+}
---
+++ Templates/Basic/Server/ViewModels/LineChartExample .cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading;
+using Basic.Shared;
+using DotNetify;
+
+
+namespace Basic.Server
+{
+    public class LineChartExample : BaseVM
+    {
+        
+        public int[] Waveform
+        {
+            get => Get<int[]>();
+            set => Set(value);
+        }
+  
+        public LineChartExample()
+        {
+
+            var timer = Observable.Interval(TimeSpan.FromSeconds(1));
+            var random = new Random();
+
+            Waveform = Enumerable.Range(1, 8).Select(_ => random.Next(500, 1000)).ToArray();
+
+
+            timer.Subscribe(x =>
+            {
+                x += 31;
+                this.AddList(nameof(Waveform), new int[] { random.Next(500, 1000) });
+
+                PushUpdates();
+            });
+
+        }
+
+
+
+
+
+
+
+    }
+}
